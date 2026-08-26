6a98016c7ab6aeef5fe06c1e8dc5ed643c55003b
unknown <kybra.korkmaz@gmail.com>
2026-08-27 00:08:51 +0300

    dark and light mode palette

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,24 @@
-import { Stack } from "expo-router";
+import { ClerkProvider } from '@clerk/expo'
+import { tokenCache } from '@clerk/expo/token-cache'
+import {DarkTheme, DefaultTheme, Stack, ThemeProvider} from "expo-router";
 import "../../global.css";
-export default function RootLayout() {
-  return <Stack screenOptions={{headerShown:false}}/>;
+import {useColorScheme} from "react-native";
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+if (!publishableKey) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
 }
+
+export default function RootLayout() {
+    const colorScheme = useColorScheme();
+  return(
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+        <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+            <Stack screenOptions={{headerShown:false}}/>
+        </ThemeProvider>
+    </ClerkProvider>
+  )
+}
+
+ // todo:stack and slot
